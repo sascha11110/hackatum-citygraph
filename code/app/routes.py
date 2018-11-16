eb5cb9b4f12e811f48ed7996d94d18dc55e9d9b5
Sascha Beele <sascha.beele@startmail.com>
2018-11-16 23:50:50 +0100

    Add basic templating

--- code/app/routes.py
+++ code/app/routes.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+
+@app.route('/proposal')
+def proposal():
+    return render_template('timeline.html')
