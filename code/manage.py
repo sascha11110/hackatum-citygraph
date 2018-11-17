6df6d357e20717e0ab74125edb697bb2fefce2c1
Ponitac <uholtgrave@googlemail.com>
2018-11-17 10:15:28 +0100

    First Try database Integration

--- code/manage.py
+++ code/manage.py
@@ -1,12 +1,22 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db
+import flaskext.couchdb
 
 
-manager = Manager(app)
-migrate = Migrate(app, db)
 
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
+    
+    app.config.update(
+        DEBUG = True,
+        COUCHDB_SERVER = 'http://localhost:5432/',
+        COUCHDB_DATABASE = 'hackatum'
+    )
+    manager = flaskext.couchdb.CouchDBManager()
+    migrate = Migrate(app, db)
+    manager.add_viewdef(docs_by_author)
+    manager.sync(app)
+
+    manager.add_command('db', MigrateCommand)
     manager.run()
